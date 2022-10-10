fab5fcde5e51ebfddac1df60d38af5bd3bdfcd1d
JEFFERYBOASTJR <jjb8019980@LPSSONLINE.COM>
2022-10-10 18:10:00 +0000

    <message about commit>

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -40,7 +40,12 @@
 
     // example:
     createCollectable(type.steve, 200, 170, 6, 0.7);
-
+    createCollectable(type.db, 300, 170, 6, 0.7);  // worth 10 points
+    createCollectable(type.max, 400, 170, 6, 0.7); // worth 20 points
+    createCollectable(type.steve, 350, 170, 6, 0.7); // worth 30 points
+    createCollectable(type.grace, 100, 170, 6, 0.7); // worth 40 points
+    createCollectable(type.kennedi, 250, 170, 6, 0.7); // worth 50 points
+ 
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
   }
--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -28,7 +28,10 @@
 
     ////////////////////////////////////////////////////////////////////////
     // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
-
+    createPlatform(0, 600);
+    createPlatform(0,500);
+    createPlatform(500, 550);
+    
     /*
      * ground : here, we create a floor. Given the width of of the platform
      * asset, giving it a scaleX and scaleY of 2 will stretch it across the
