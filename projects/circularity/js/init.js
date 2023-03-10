a9c3d67803f949e61e1c02ac13d01b6806ac53db
JEFFERYBOASTJR <jjb8019980@LPSSONLINE.COM>
2023-03-10 19:17:28 +0000

    update

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -121,7 +121,7 @@ var init = function (window) {
         
         app.addUpdateable(window.opspark.game);
     }
-};
+;
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
